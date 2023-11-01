61d9defef3665ee3a7323c36b754b2d3d3ca741b
Skyba Evhen <evad.se.dev@gmail.com>
2023-11-01 18:42:55 +0200

    версия для реального примера

--- drawio/draw-io-demo/src/main/java/org/example/App.java
+++ drawio/draw-io-demo/src/main/java/org/example/App.java
@@ -1,6 +1,12 @@
 package org.example;
 
 
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.text.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Entities;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 import org.nasdanika.drawio.*;
 import org.w3c.dom.NamedNodeMap;
 import org.xml.sax.SAXException;
@@ -9,8 +15,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,52 +25,81 @@ import java.util.Set;
  * Hello world!
  * //Build-Jdk: 11.0.15
  */
-public class App 
-{
-    public static void main( String[] args ) throws ParserConfigurationException, IOException, TransformerException {
+public class App {
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        ///  app.readDiagramAndRewrite();
+        //app.testCreate();
 
 
-            App app = new App();
-            app.test();
+        String fileText;
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+        try {
+            inputStream = App.class.getClassLoader().getResourceAsStream("compressed-html.txt");
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            // app.testCreate();
+
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            fileText = builder.toString();
+
+        } finally {
+            bufferedReader.close();
+            inputStream.close();
+        }
+
+        org.jsoup.nodes.Document newDocument = Jsoup.parse(fileText.replace("\\", ""), "", Parser.htmlParser());
+        newDocument.outputSettings().escapeMode(Entities.EscapeMode.base);
+        newDocument.outputSettings().charset(CharEncoding.UTF_8);
+
+        Elements select = newDocument.select("div.mxgraph");
+        String attr = select.get(0).attr("data-mxgraph");
+
+
+        int diagram = attr.indexOf("<diagram");
+        int diagramEnd = attr.indexOf("</diagram>");
+
+        String substring2 = attr.substring(diagram, diagramEnd + "</diagram>".length());
+        String str = "<mxfile>" + substring2 + "</mxfile>";
+        readFromHtml(str);
     }
 
-    public void test ()   {
-        try {
-            URL resource = getClass().getClassLoader().getResource("compressed.drawio");
 
-                       Document document = Document.load(resource);
+    public static void readFromHtml(String htmlData)  {
+        try {
+            Document document = Document.load(htmlData, null);
             List<Page> pages = document.getPages();
             Page page = pages.get(0);
 
             Model model = page.getModel();
             Root root = model.getRoot();
             List<Layer> layers = root.getLayers();
-
             //первый слой
             Layer layer = layers.get(0);
-            List<ModelElement> modelElements = layer.getElements();
-
+            List<LayerElement> layerElements = layer.getElements();
             // NodeImpl
-            for(ModelElement modelElement :  modelElements ){
+            for (ModelElement modelElement : layerElements) {
 
                 NamedNodeMap attributes = modelElement.getElement().getAttributes();
 
                 int length = modelElement.getElement().getAttributes().getLength();
                 // добрались до того места где лига-закон
                 org.w3c.dom.Node value1 = attributes.getNamedItem("value");
-                if(value1!= null) {
+                if (value1 != null) {
                     String value = attributes.getNamedItem("value").toString();
                     String replacedString = replaceToCorrectServer(value);
                     attributes.getNamedItem("value").setNodeValue(replacedString);
-                    int aa = 0;
                 }
             }
 
-            // сохранить розархивируванную
+            // сохранить роз-архивируванную
             String save2 = document.save(false);
-            writeUsingOutputStream(save2,"uncompressed_server.drawio");
+            writeUsingOutputStream(save2, "real_diagram.drawio");
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -74,25 +109,72 @@ public class App
             throw new RuntimeException(e);
         }
 
-        System.out.println( "Hello World!" );
     }
 
-    private String replaceToCorrectServer(String value) {
+    private static String replaceToCorrectServer(String value) {
         String oldServer = "https://ips.ligazakon.net";
         String newServer = "https://legislation-dev.ligaunited.net";
 
         int pos = value.indexOf(oldServer);
         if (pos != -1) {
-            StringBuffer textBuff = new StringBuffer(value);
-            textBuff = textBuff.replace(pos, pos + oldServer.length(), newServer);
+            StringBuilder textBuff = new StringBuilder(value);
+            textBuff.replace(pos, pos + oldServer.length(), newServer);
             value = textBuff.toString();
         }
-
 
         return value;
     }
 
-    public void testCreate () throws ParserConfigurationException, IOException, TransformerException {
+    public void readDiagramAndRewrite() {
+        try {
+            URL resource = getClass().getClassLoader().getResource("compressed.drawio");
+
+            Document document = Document.load(resource);
+            List<Page> pages = document.getPages();
+            if(pages.isEmpty()){
+                // log
+                return;
+            }
+            Page page = pages.get(0);
+
+            Model model = page.getModel();
+            Root root = model.getRoot();
+            List<Layer> layers = root.getLayers();
+
+            //первый слой
+            Layer layer = layers.get(0);
+            List<LayerElement> layerElements = layer.getElements();
+
+            // NodeImpl
+            for (ModelElement modelElement : layerElements) {
+
+                NamedNodeMap attributes = modelElement.getElement().getAttributes();
+
+                int length = modelElement.getElement().getAttributes().getLength();
+                // добрались до того места где лига-закон
+                org.w3c.dom.Node value1 = attributes.getNamedItem("value");
+                if (value1 != null) {
+                    String value = attributes.getNamedItem("value").toString();
+                    String replacedString = replaceToCorrectServer(value);
+                    attributes.getNamedItem("value").setNodeValue(replacedString);
+                }
+            }
+            // сохранить роз-архивируванную
+            String save2 = document.save(false);
+            writeUsingOutputStream(save2, "uncompressed_server.drawio");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParserConfigurationException | TransformerException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Hello World!");
+    }
+
+    public void testCreate() throws ParserConfigurationException, IOException, TransformerException {
         Document document = Document.create(false, null);
         Page page = document.createPage();
         page.setName("My first new page");
@@ -132,29 +214,17 @@ public class App
         connectionStyle.put("jettySize", "auto");
         connectionStyle.put("html", "1");
 
-      //  Files.writeString(new File("new-uncompressed.drawio").toPath(), document.save(null));
-
-
-        Path path = new File("new-compressed.drawio").toPath();
-        String save2 = document.save(true);
-        writeUsingOutputStream(save2,"new-uncompressed.drawio");
+        String save2 = document.save(false);
+        writeUsingOutputStream(save2, "new-uncompressed.drawio");
 
     }
 
-    private  void writeUsingOutputStream(String data, String fileName) {
-        URL resource = getClass().getClassLoader().getResource(fileName);
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(fileName);
-            os.write(data.getBytes(), 0, data.length());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                os.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    private static void writeUsingOutputStream(String data, String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        writer.write(data);
+
+        writer.close();
+
+
     }
 }

b30a9924abbfa2417d188c1f25435faa198573ac
Skyba Evhen <evad.se.dev@gmail.com>
2024-01-25 14:18:07 +0200

    regexp demos

--- regexp/regexp-base/src/main/java/com/se/sample/AppGreedyLazy.java
+++ regexp/regexp-base/src/main/java/com/se/sample/AppGreedyLazy.java
@@ -9,11 +9,92 @@ import java.util.regex.Pattern;
 public class AppGreedyLazy {
 
     public static void main(String[] args) {
+       demoHtml1();
+       // demoApp1();
+       // demoApp2();
+       // demoApp3();
+       // demoGreedyLazy();
+        backReferenceDemo();
+    }
 
+    private static void backReferenceDemo() {
+        /*
+        [ ]+ → один или несколько пробелов, так мы ограничиваем слово. В принципе, тут можно заменить на метасимвол \b.
+        (\w+) → любой буквенный или цифровой символ, или знак подчеркивания. Квантификатор «+» означает, что символ должен идти минимум один раз. А то, что мы взяли все это выражение в круглые скобки, говорит о том, что это группа. Зачем она нужна, мы пока не знаем, ведь рядом с ней нет квантификатора. Значит, не для повторения. Но в любом случае, найденный символ или слово — это группа 1.
+        [ ]+ → снова один или несколько пробелов.
+        \1 → повторение группы 1. Это и есть ссылка
+         */
+        runTest("[ ]+(\\w+)[ ]+", "Поздравляем! Вы прошли на на новый уровень. Так что что улыбаемся и и машем.");
 
     }
 
-    private static int runTest(String patternStr, String inputString, String comment) {
+    private static void demoGreedyLazy() {
+        System.out.println("квалификаторы");
+
+        // находит самую длинную последовательность символов, завершающуюся на ox
+        System.out.println("поглощает всю строку ");
+        System.out.println("после чего откатывается вплоть до обнаружения того, что входной текст заканчивается этими символам");
+        runTest(".*ox", " fox box pox", "наглядный пример жадного");
+
+        //находит самую короткую последовательность символов, завершающуюся на ox
+        runTest(".*?ox", " fox box pox", "наглядный пример ленивого");
+
+        /*
+         *  он поглощает весь входной текст и не остается ничего, что могло бы соответствовать ox
+         *  в конце регулярного выражения
+         */
+        runTest(".*+ox", " fox box pox", "сверх-жадный квантификатор");
+
+    }
+
+
+
+    private static void demoApp2() {
+    }
+
+    private static void demoHtml1() {
+        String inputXmlString = "<req> <query>Ан</query> <gender>FEMALE</gender></req>";
+        String result = inputXmlString.replaceAll("<.*>","");
+        System.out.println(result);
+        System.out.println("==============================================");
+
+        int i = runTest("<.*?>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>");
+         i = runTest("<.*>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>");
+
+        System.out.println(result);
+        System.out.println("==============================================");
+
+        inputXmlString = "<req> <query>Ан</query> <gender>FEMALE</gender></req>";
+        result = inputXmlString.replaceAll("<[^>]*>","");
+        System.out.println(result);
+        System.out.println("==============================================");
+
+        inputXmlString = "<query>Ан</query> <gender>FEMALE</gender></req>";
+        result = inputXmlString.replaceAll("<[^>]*>|<[^>\\/]\\/>","");
+        System.out.println(result);
+
+        inputXmlString = "<req> <query>Ан</query> <gender>FEMALE</gender></req>";
+        result = inputXmlString.replaceAll("/<(\\/?)[a-zA-Z]+(?:[^>\"']+|\"[^\"]*\"|'[^']*')*>/g","");
+        System.out.println(result);
+        System.out.println("==============================================");
+
+    }
+
+    private static void demoApp1() {
+
+        // регулярное выражение возвращает целую строку, внутри которой есть несколько тегов.
+        runTest("<.*>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>", "первый вариант");
+        System.out.println("ленивый вместо жадного");
+        runTest("<.*>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>", "первый вариант");
+
+        //начинается на < внутри нет >
+        runTest("<[^>]+?>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>", "первый вариант");
+
+        runTest("<[^>]*>", "<req> <query>Ан</query> <gender>FEMALE</gender></req>", "первый вариант");
+
+    }
+
+    public static int runTest(String patternStr, String inputString, String comment) {
         if (comment != null && comment.length() > 0) {
             System.out.println(comment);
         }
@@ -37,7 +118,15 @@ public class AppGreedyLazy {
     }
 
 
-    private static int runTest(String patternStr, String inputString) {
+    public static int runTest(String patternStr, String inputString) {
         return runTest(patternStr, inputString, "");
+    }
+
+    private static void demoApp3() {
+
+        runTest("^The\\w{0,1}", "Therefore" );
+        runTest("^The\\w{0,1}", "The one " );
+        runTest("^[The]\\w{0,1}", "The fore" );
+        int a =0;
     }
 }
--- regexp/regexp-base/src/main/java/com/se/sample/Demos.java
+++ regexp/regexp-base/src/main/java/com/se/sample/Demos.java
@@ -3,8 +3,15 @@ package com.se.sample;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.se.sample.AppGreedyLazy.runTest;
+
 public class Demos {
 
+    final static String resSnipet2 = ";keep-together:always;}\n" +
+            ".t1{table-layout:fixed;border-collapse:collapse;border-spacing:0;}\n" +
+            "</style>\n" +
+            "<title><span id='snippet_1092'></span><snippet class=\"found\" style=\"background:#2196f3\">ДОГОВІР</snippet> N</title>\n" +
+            "<meta content=\"Volodyko-N\" name=\"author\">\n" ;
     public static void main(String[] args) {
 
 
@@ -13,6 +20,16 @@ public class Demos {
         checkExpression("(text 99)");
 
         System.out.println("bye");
+
+
+        String pattern = ";[a-z]+[^><]+((?=<)|(?!.*>))";
+        String text = resSnipet2;
+
+    //    int aa = AppGreedyLazy.runTest(pattern,  text, "");
+        String s = text.replaceAll(pattern, "1111");
+        int a =0;
+
+
     }
 
     private static boolean checkExpression(String input) {

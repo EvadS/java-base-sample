32a456b563541180630e12cf2d1c65e8418d0862
Yevhen Skyba <yevhen.skyba@ligazakon.ua>
2024-03-11 22:36:40 +0200

    first working version

--- struct/tree-demo/src/main/java/com/se/sample/formatter/QueryResultFormatter.java
+++ struct/tree-demo/src/main/java/com/se/sample/formatter/QueryResultFormatter.java
@@ -1,13 +1,12 @@
 package com.se.sample.formatter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.se.sample.utils.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.*;
+
 
 public class QueryResultFormatter {
 
@@ -21,97 +20,121 @@ public class QueryResultFormatter {
     private static final Logger logger = LogManager.getLogger(QueryResultFormatter.class);
 
     public static void main(String[] args) throws IOException {
-        List<Map<String, Object>> maps = FileUtils.readListMap("f100_records.json");
+        List<Map<String, Object>> maps = FileUtils.readListMap("f100_records1.json");
 
         List<Map<String, Object>> childs = new QueryResultFormatter().getChilds(maps, "", 3);
 
     }
 
+    private static List<Map<String, Object>> queryForList(String s) {
+        final List<Map<String, Object>> result = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            final Map<String, Object> map = new HashMap<>();
+            map.put("key", "key" + i);
+            map.put("value", "value" + i);
+            result.add(map);
+        }
+
+        return result;
+    }
+
     // TODO: WORKING HERE
     public List<Map<String, Object>> getChilds(List<Map<String, Object>> aAllRecords, final String strParentID, final int nLevelCount) {
-        if (nLevelCount <= 0)
+       // если ничего не нашли на предыдущем шаге
+       if (nLevelCount <= 0)
             return null;
 
+        // результат
         final List<Map<String, Object>> oResult = new LinkedList<Map<String, Object>>();
-
         Map<String, Object> root = new HashMap<>();
 
-
-        int row = 1;
-        // перебираем все записи
+        // вспомагательная структура - ускоряем поиск родительских обьектов
+        // вместо того чтобы перебирать варианты прохоходом по елементам
+        Map<String, Map<String, Object>> map = new HashMap<>();
         for (Map<String, Object> record : aAllRecords) {
+            String id = record.get(strIDFieldName).toString();
+            map.put(id, record);
+        }
 
-            String id = String.valueOf(record.get(strIDFieldName));
+        // перебираем все записи
+        for (Map<String, Object> item : aAllRecords) {
+
+            String id = String.valueOf(item.get(strIDFieldName));
+            // получим все уровни
             String[] split = id.split("\\.");
 
-            Map<String, Object> parentCurr = root;
+            if(split.length> nLevelCount){
+                // превысили допустимое количество вложений (ограничение по уровням)
+                continue;
+            }
+
+            Map<String, Object> currentParent  = root;
             String parentId = id;
+
             // ищем родетеля для текущего
             for (int i = 0; i < split.length - 1; i++) {
-                //search parent
-                if (i == 0) {
-                    parentId = split[i];
-                } else {
-                    parentId +=  "." + split[i];
-                }
+                parentId = i == 0 ? split[i] : (parentId += "." + split[i]);
 
-                // TODO: проверить что для єтого есть родитель
-                parentCurr = getParentCurr(root, parentId, i);
+                // TODO: проверить что для этого есть родитель
+                currentParent = getParentElementByPArentId(map, parentId);
             }
 
             if (!id.equals(parentId)) {
-                if (parentCurr.get(CHILDS_KEY) == null) {
-                    parentCurr.put(CHILDS_KEY, new HashMap<String, Object>());
+                if (currentParent .get(CHILDS_KEY) == null) {
+                    currentParent .put(CHILDS_KEY, new HashMap<String, Object>());
                 }
-                ((Map<String, Object>) parentCurr.get(CHILDS_KEY)).put(id, record);
-            } else { // id: 032-0000
-                parentCurr.put(id, record);
-                oResult.add(parentCurr);
+                ((Map<String, Object>) currentParent .get(CHILDS_KEY)).put(id, item);
+            } else {
+                currentParent.put(id, item);
             }
-
-            if (row == 10) {
-                printToConsole(root);
-            }
-            row++;
         }
 
-        // TODO: stub
-        return (List<Map<String, Object>>) root.get("032-0000");
+        // приводим в нужный результат
+
+        Iterator<Object> iterator = root.values().iterator();
+
+        while (iterator.hasNext()){
+            Map<String, Object> next = (Map<String, Object>)iterator.next();
+            oResult.add(next);
+        }
+
+        return oResult;
     }
 
-    private void printToConsole(Map<String, Object> root) {
-        try {
-            String jsonString = new ObjectMapper().writeValueAsString(root);
-            logger.info(jsonString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private Map<String, Object> getParentCurr(Map<String, Object> root, String parentId, int level) {
 
-       if (level == 1) {
-            // 032-0000.0000 - ищем в child
-            // сначала взять дочерний потом в дочерний ложить по ключу
-            Map<String, Object> childs =(Map<String, Object>)((Map<String, Object>) (Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) root.get("032-0000")).get("childs")).get("032-0000.0000"));
-            return childs;
+    private Map<String, Object> getParentElementByPArentId(Map<String, Map<String, Object>> map, String parentId) {
 
-        } else if (level == 2) {
-            Map<String, Object> childs = (Map<String, Object>)(((Map<String, Object>)(Map<String, Object>)((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) root.get("032-0000")).get("childs"))
-                    .get("032-0000.0000")).get("childs")).get("032-0000.0000.0000"));
-            return childs;
-        } else if (level == 3) {
-            Map<String, Object> childs =(Map<String, Object>)((Map<String, Object>) ((Map<String, Object>)((Map<String, Object>)((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>)
-                    root.get("032-0000")).get("childs")).get("032-0000.0000")).get("childs")).get("032-0000.0000.0000")).get("childs") ).get("032-0000.0000.0000.0000") ;
-            return childs;
-        } else if (level == 4) {
-            Map<String, Object> child1 = (Map<String, Object>) ((Map<String, Object>) root.get("032-0000")).get("childs");
-            return child1;
-        }
+        // если не использовать мапу для поиска родительского - получим следующий код
+//      if (level == 1) {
+//            // 032-0000.0000 - ищем в child
+//            Map<String, Object> childs = (Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) root
+//            .get("032-0000")).get("childs"))
+//            .get("032-0000.0000");
+//            return childs;
+//
+//        } else if (level == 2) {
+//            Map<String, Object> childs = (Map<String, Object>) (((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) root
+//                    .get("032-0000")).get("childs"))
+//                    .get("032-0000.0000")).get("childs"))
+//                    .get("032-0000.0000.0000"));
+//            return childs;
 
-        Map<String, Object> stringObjectMap = (Map<String, Object>) root.get(parentId);
+//        } else if (level == 3) {
+//            Map<String, Object> childs = (Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>)
+//                    root
+//                    .get("032-0000")).get("childs"))
+//                    .get("032-0000.0000")).get("childs")).get("032-0000.0000.0000"))
+//                    .get("childs")).get("032-0000.0000.0000.0000");
+//            return childs;
+//        } else if (level == 4) {
+//            Map<String, Object> child1 = (Map<String, Object>) ((Map<String, Object>) root.get("032-0000")).get("childs");
+//            return child1;
+//        }
+
+        Map<String, Object> stringObjectMap = map.get(parentId);
         return stringObjectMap;
-
     }
 
 }

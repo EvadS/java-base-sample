070f70731a0ca0371e1c0341e600ebf77690b578
Скиба Євген <yevhen.skyba@ligazakon.ua>
2026-02-20 17:31:50 +0200

    keep working with json

--- json/json-map-demos/src/main/java/com/se/sample/App.java
+++ json/json-map-demos/src/main/java/com/se/sample/App.java
@@ -1,5 +1,12 @@
 package com.se.sample;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,10 @@ public class App
 {
     public static void main( String[] args )
     {
+
+
+
+
         System.out.println( "Hello World!" );
     }
 }
--- json/json-map-demos/src/main/java/com/se/sample/ipsu/ApplicationDemo.java
+++ json/json-map-demos/src/main/java/com/se/sample/ipsu/ApplicationDemo.java
@@ -1,14 +1,15 @@
 package com.se.sample.ipsu;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import org.apache.commons.lang3.StringUtils;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class ApplicationDemo {
 
@@ -16,12 +17,13 @@ public class ApplicationDemo {
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
 
         InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(THIRD_JSON_FILE_NAME);
+        List<Object> value = readJsonAsString(THIRD_JSON_FILE_NAME);
 
-        String jsonText = readJsonAsString(THIRD_JSON_FILE_NAME);
-        List<ReferenceText> list = bindReferenceList(jsonText);
+        List<ReferenceText> list = bindReferenceList(THIRD_JSON_FILE_NAME);
+        List<ReferenceText> list2 = bindReferenceList2(THIRD_JSON_FILE_NAME);
 
 
         int a = 0;
@@ -63,22 +65,26 @@ public class ApplicationDemo {
         return "";
     }
 
-    private static String readJsonAsString(String fileName) {
+    private static List<Object> readJsonAsString(String fileName) {
         // Assumes "foo.json" is directly under the "resources" folder
 
         try (InputStream is = ApplicationDemo.class.getResourceAsStream("/" + fileName)) {
-            return objectMapper.readValue(is, String.class);
+            return objectMapper.readValue(is, new TypeReference<List<Object>>() {
+            });
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return "";
+        return Collections.emptyList();
     }
 
-    public static List<ReferenceText> bindReferenceList(String jsonString) throws JsonProcessingException {
+    public static List<ReferenceText> bindReferenceList(String fileName) throws JsonProcessingException {
         List<ReferenceText> result = new ArrayList<>();
 
-        JsonNode rootNode = objectMapper.readTree(jsonString);
+        try (InputStream is= ApplicationDemo.class.getResourceAsStream("/" + fileName)){
+
+            JsonNode rootNode = objectMapper.readTree(is);
         if (rootNode.isArray()) {
             for (JsonNode node : rootNode) {
 
@@ -97,6 +103,85 @@ public class ApplicationDemo {
                 }
             }
         }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return result;
     }
+
+
+    public static List<ReferenceText> bindReferenceList2(String jsonString) throws JsonProcessingException {
+        List<ReferenceText> result = new ArrayList<>();
+        // ----------------------------------------------------------------------------------------------
+
+        try (InputStream is = ApplicationDemo.class.getResourceAsStream("/" + THIRD_JSON_FILE_NAME)) {
+
+
+            JsonNode rootNode2 = objectMapper.readTree(is);
+            JsonNode contacts = rootNode2.path("documents");
+
+
+            List<JsonNode> documents = StreamSupport.stream(rootNode2.spliterator(), false)
+                    .map(i -> i.get("documents"))
+                    .collect(Collectors.toList());
+
+            List<ReferenceText> documents1 = StreamSupport.stream(rootNode2.spliterator(), false)
+                    .flatMap(i -> StreamSupport.stream(i.get("documents").spliterator(), false)
+                            .map(j -> bindReference(j)))
+                    .collect(Collectors.toList());
+
+            // here
+            List<JsonNode> jsonDocuments = StreamSupport.stream(rootNode2.spliterator(), false)
+                    .flatMap(i -> StreamSupport.stream(i.get("documents").spliterator(), false)
+                            .map(j -> bindJonNode(j)))
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+              return result;
+    }
+
+    private static ReferenceText bindReference(JsonNode jsonNode) {
+
+        String title = jsonNode.get("title").asText();
+
+        JsonNode referenceNode = jsonNode.get("reference");
+        String id = referenceNode.get("id").asText();
+        String an = "";
+
+        if (referenceNode.has("an")) {
+            an = referenceNode.get("id").asText();
+        }
+
+        return new ReferenceText(id, an, title);
+    }
+
+
+    private static JsonNode bindJonNode(JsonNode rootNode) {
+
+        Set<String> targetFields = new HashSet<>();
+
+        // Extract required fields
+        // String id = rootNode.path("id").asText();
+        JsonNode reference = rootNode.path("reference");
+        JsonNode title = rootNode.path("title");
+        String id = reference.get("id").asText();
+        String an = "";
+        if(reference.has("an")){
+            an = reference.get("an").asText();
+        }
+
+        // Create a new JsonNode for the transformed request
+        ObjectNode transformedRequest = objectMapper.createObjectNode();
+        transformedRequest.put("id", id);
+        transformedRequest.put("an", an);
+        transformedRequest.put("title", title);
+
+        return transformedRequest;
+    }
+
+
 }
